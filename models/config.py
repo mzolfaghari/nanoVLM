c0f206c351b997070fc04398ed1b363a53fc05d3
Reza <mohamadreza.zolfaghari@gmail.com>
2026-05-15 15:34:31 +0200

    rollback config changes

--- models/config.py
+++ models/config.py
@@ -59,7 +59,7 @@ class TrainConfig:
     lr_mp: float = 0.00512
     lr_vision_backbone: float = 5e-5 #0.0005 #
     lr_language_backbone: float = 5e-5 #0
-    val_size: int = 512
+    val_size: int = 50000
     batch_size: int = 2
     gradient_accumulation_steps: int = 8
     max_grad_norm: float = 1.0
@@ -73,7 +73,7 @@ class TrainConfig:
     compile: bool = False
     resume_from_vlm_checkpoint: bool = False # Indicate if the training should be resumed from a checkpoint of the whole VLM or you want to start from scratch
     train_dataset_path: str = 'HuggingFaceM4/FineVision_concat_shuffled_2'
-    train_dataset_name: tuple[str, ...] = ("scienceqa", ) # use "default" for FineVision, "all" for all cauldron subsets
+    train_dataset_name: tuple[str, ...] = ("default", ) #('allava_laion', 'allava_vflan', 'cambrian(filtered)_processed', 'LLaVA_Instruct_150K', 'mmevol', 'sharegpt4o', 'sharegpt4v(coco)', 'sharegpt4v(knowledge)', 'sharegpt4v(llava)', 'sharegpt4v(sam)') # 'vision_flan(filtered)', 'lvis_instruct4v',
     stream_dataset: bool = True
     relevance_min_rating: int = 1
     image_correspondence_min_rating: int = 1
@@ -81,7 +81,7 @@ class TrainConfig:
     formatting_min_rating: int = 1
     wandb_entity: str = "HuggingFace" # Indicate the entity to log to in wandb
     log_wandb: bool = True
-    use_lmms_eval: bool = False # Use lmms-eval for evaluation
+    use_lmms_eval: bool = True # Use lmms-eval for evaluation
     lmms_eval_tasks: str = 'mmstar,mmmu_val,ocrbench,textvqa_val,docvqa_val,scienceqa,mme,infovqa_val,chartqa' # Pass additional task as one string, seperated by commas without spaces (e.g. 'mmstar,mmmu,ocrbench')
     lmms_eval_limit: float = None
     lmms_eval_batch_size: int = 64
